fd33a886ede2c6f7ee9ef6ef2bf14e981ad611a1
zbruennig <zkb@ku.edu>
2017-02-05 18:42:07 -0600

    Add files via upload
    Provided files for this class. Chalk is an introductory library NOT DESIGNED BY ME that we've been told to use for our homeworks. All credit for these goes to Andy Gill: http://ku-fpg.github.io/people/andygill/

---
+++ chalk.HW1.js
@@ -0,0 +1,68 @@
+// Chalk - EECS 368's library for teaching and understanding JavaScript
+// Version HW1 (S17) with monospace added (as suggested by Zane J Cersovsky)
+//
+var chalk = {
+    print: function(str) {
+            $("#chalk").append(str.replace(/&/g, '&amp;')
+                                  .replace(/</g, '&lt;')
+                                  .replace(/>/g, '&gt;')
+			          .replace(/\ /g,'&nbsp;'));
+    },
+    newline: function() {
+            $("#chalk").append("<BR/>");
+    },
+    hr: function() {
+            $("#chalk").append("<hr/>");
+    },
+    println: function(str) {
+            chalk.print(str);
+            chalk.newline();
+    },
+    canvas: function(w,h) {
+	var canvas = $("<canvas/>");
+	$("#chalk").append(canvas);
+	$("#chalk").append("<BR>");
+	canvas.css("border","1pt solid black");
+	canvas.css("background","white");
+	canvas.attr("width",w);
+	canvas.attr("height",h);
+	return canvas[0].getContext("2d");
+    },
+    callback: function(o) {
+	switch (typeof o) {
+	case "object":
+	    if (typeof (o.entrybox) == "function") {
+		var input = $("<input/>");
+		$("#chalk").append(input);
+		$("#chalk").append("<BR>");
+		input.attr("size",72);
+		input.css("color","red");
+		input.focus();
+		input.on('keyup',function(e) { 
+			if (e.keyCode == 13) {
+			    input.off(); 
+			    var str = input.val();
+			    input.attr("disabled",true);
+			    input.css("background","#f8f8f8");
+			    chalk.callback(o.entrybox(str));
+			}
+		    });
+	    }
+	    if (typeof (o.refresh) == "function") {
+		setTimeout(function() { chalk.callback(o.refresh()); },20);
+	    }
+	break;
+	default:
+	    break;
+	}
+    }
+}
+
+
+// Do this when document is ready
+$(function(){
+        $("#chalk").css("border","solid black 1px");
+        $("#chalk").css("font-family","monospace");
+        $("#chalk").css("background","#eeeeee");
+        chalk.callback(main());
+});        
